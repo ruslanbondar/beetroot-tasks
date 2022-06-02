2c68df780953f80c47369cac27bf071a70c656fd
ruslanbondar <mailofbondik@gmail.com>
2022-06-02 22:01:21 +0300

    js-2

---
+++ js-2_data-types_operators/data-types.js
@@ -0,0 +1,56 @@
+// string
+let str = 'I am student of Beetroot Academy';
+// console.log(str.length);
+// console.log(str.includes('of q'));
+// console.log(str.trim());
+// console.log(str.toLowerCase());
+// console.log(str.);
+// console.log(str.concat(' ', str, ' !!!'));
+
+// number
+// let num = 5.022323;
+// let dollar = prompt('How much money you want:');
+// const value = 0.933049;
+// let result = dollar * value;
+// alert(result.toFixed(0));
+// console.log(num.toFixed(2));
+// console.log(isNaN('12'));
+
+// Math
+// let num = 1.4;
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// let maxValue = Math.max(1, 4, 8, 3);
+// console.log(maxValue);
+// console.log(Math.min(4, 5, -1, 0));
+// console.log(Math.pow(9, 4));
+// console.log(Math.sqrt(121));
+
+// bigInt
+// const bugNum = 9007199254740991n;
+
+// boolean
+// let value = 'fdfdfdf';
+// console.log(Boolean(value));
+// console.log(!!value);
+
+// null
+
+// undefined
+let value;
+console.log();
+
+// symbol
+// let id1 = Symbol('id');
+// let id2 = Symbol('id');
+// console.log(id1);
+// console.log(id2);
+
+// object
+let obj = {
+  a: 1,
+  b: 4,
+  id: '10',
+  c: true,
+};
+console.log(obj.id);
---
+++ js-2_data-types_operators/operators.js
@@ -0,0 +1,54 @@
+// a + b;
+// a - b;
+// a / b;
+// a * b;
+
+// console.log(5 ** 3); // піднесення до ступіня
+
+// let a = 4;
+// a = a + 5; // спочатку виконується вираз справа, а потім відбувається присвоєння в змінну
+// a += 5; // аналог виразу вище
+// console.log(a);
+
+// let partition = 30 % 4; // результат остачі від ділення
+// console.log(partition);
+
+// let num1 = 0;
+// num1++;
+// num1++;
+// num1++;
+// console.log(num1);
+// console.log(num1);
+// console.log(num1++);
+// console.log(num1++);
+// console.log(num1++);
+// console.log(num1);
+// let sum = num1 + 5;
+// console.log(sum);
+
+// let num2 = 0;
+// console.log(num2);
+// console.log(++num2);
+// console.log(++num2);
+// console.log(++num2);
+// console.log(++num2);
+// ++num2;
+// console.log(num2);
+
+// let a = 7;
+// let b = 7;
+// console.log(a == b);
+// console.log(a === b);
+// console.log(a != b);
+// console.log(a !== b);
+// console.log(a >= b);
+
+
+// let result = (4 < 6) && (3 > 5);
+// console.log(result);
+
+// let result2 = (4 < 6) && (3 > 5) || (7 > 6);
+// console.log(result2);
+
+let str = '';
+console.log(!str);
